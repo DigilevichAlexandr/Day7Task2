ade5f3b1f916ac62c49511f1631bac32ef74c125
Alexandr <digilevich.alexandr@gmail.com>
2015-07-29 13:07:47 +0300

    1

---
+++ Day7Task2/Fibonacci.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7Task2
+{
+    class Fibonacci:IEnumerable<int>
+    {
+        //public static IEnumerator<int> GetNumber()
+        //{
+
+        //}
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            int number1 = 0;
+            yield return number1;
+            int number2  = 1;
+            yield return number2;
+            int number3;
+            while(true)
+            {
+                number3 = number1 + number2;
+                yield return number3;
+                number1 = number2;
+                number2 = number3;
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Day7Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo cki;
+            Console.TreatControlCAsInput = true;
+            Fibonacci f = new Fibonacci();
+            Console.WriteLine("press (Esc) key to exit");
+            Console.WriteLine("press somthig else to get one more number");
+            foreach(int i in f)
+            {
+                cki = Console.ReadKey();
+                if ((cki.Key == ConsoleKey.Escape))
+                    break;
+                Console.WriteLine("{0}",i.ToString());
+            }
+        }
+    }
+}
